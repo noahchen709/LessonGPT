2553602fd5a3a0cbca6bc11212a8c6a18944922d
noahchen709 <119009420+noahchen709@users.noreply.github.com>
2025-05-31 14:33:11 +0800

    Set up express and reformat

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,29 +1,46 @@
-// Generate content
+import express from "express";
+import fileUpload from "express-fileupload";
+import cors from "cors";
 import { GoogleGenAI, Type } from "@google/genai";
-import * as fs from 'fs';
-import { Buffer } from 'buffer';
 
-const ai = new GoogleGenAI({ apiKey: "AIzaSyBgZh7NFUiZSkzcOk__8LaQO0Vq8DYbquA" });
+import * as fs from "fs";
+import { Buffer } from "buffer";
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(fileUpload());
+
+const ai = new GoogleGenAI({
+  apiKey: "AIzaSyBgZh7NFUiZSkzcOk__8LaQO0Vq8DYbquA",
+});
 
 async function main() {
-    const contents = [
-        { text: `Generate an interactive lesson like Duolingo and Briliant for this lesson. 
+  const contents = [
+    {
+      text: `Generate an interactive lesson like Duolingo and Briliant for this lesson. 
                 Produce JSON matching this specification:
                 Step = { "id": integer, "question": string, "options": array, "answer": string, "explanation": string }
-                Return: array<Step>` },
-        {
-            inlineData: {
-                mimeType: 'application/pdf',
-                data: Buffer.from(fs.readFileSync("/Users/noahchen/Downloads/Session\ 11\ \(2025\)\ -\ Copy.pdf")).toString("base64")
-            }
-        }
-    ];
+                Return: array<Step>`,
+    },
+    {
+      inlineData: {
+        mimeType: "application/pdf",
+        data: Buffer.from(
+          fs.readFileSync(
+            "/Users/noahchen/Downloads/Session 11 (2025) - Copy.pdf"
+          )
+        ).toString("base64"),
+      },
+    },
+  ];
 
-    const response = await ai.models.generateContent({
-        model: "gemini-2.0-flash",
-        contents: contents
-    });
-    console.log(response.text);
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: contents,
+  });
+  console.log(response.text);
 }
 
 main();
